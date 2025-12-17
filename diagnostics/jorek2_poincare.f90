e25b96182cbcb79a4953590dcd7969d72adaff8e
Orin Varley <orin.varley@ipp.mpg.de>
2025-12-17 13:26:41 +0100

    Update how we run jorek2_poincare on different planes in the stellarator model

--- diagnostics/jorek2_poincare.f90
+++ diagnostics/jorek2_poincare.f90
@@ -27,7 +27,7 @@ real*8  :: tol, delta_phi, Zjac, psi_s, psi_t, R_in, Z_in, R_out, Z_out, Rmin, R
 real*8  :: small_delta, small_delta_s, small_delta_t, delta_phi_local, delta_phi_step
 real*8  :: atmp, cur_pert
 real*8  :: psi_out
-integer :: ierr
+integer :: ierr, checked_elms
 
 
 write(*,*) '***************************************'
@@ -134,10 +134,6 @@ if ( ierr == 0 ) then ! stpts file exists, use it.
     curr = nr
     
   end do
-#if STELLARATOR_MODEL
-  read(21,*) s
-  if (trim(adjustl(s)) .eq. "override_phi") P_start = 2.d0*pi*float(i_plane_rtree - 1)/float(n_period*n_plane)
-#endif
   close(21)
 
 else ! if no stpts file exists, use the following hard-coded default startpoints
@@ -202,7 +198,7 @@ endif
 !$omp shared(node_list, element_list, n_lines, R_start, Z_start, P_start, n_turn, n_phi, delta_phi, element_neighbours, ES, iplot_type) &
 !$omp private(i_lines, ip, R_out, Z_out, i_elm, s_out, t_out, ifail, R_line, Z_line, p_line, s_line, t_line, i_turn, i_phi, &
 !$omp         delta_phi_local, i_steps, delta_phi_step, delta_s, delta_t, s_mid, t_mid, p_mid, small_delta_s, small_delta_t, &
-!$omp         small_delta, R_in, Z_in, i_elm_prev, i_elm_tmp, R, Z, psi_out, Rp, Zp, Tp, Pp, i)
+!$omp         small_delta, R_in, Z_in, i_elm_prev, i_elm_tmp, R, Z, psi_out, Rp, Zp, Tp, Pp, i, checked_elms)
 
 ! --- Trace the fieldlines
 !$omp do
@@ -214,7 +210,7 @@ L_IL: do i_lines=1,n_lines
   write(*,'(1x,2(a,i6),a,2f8.3)') 'Line',i_lines,' of',n_lines,' started at',R_start(i_lines),Z_start(i_lines)
 !$omp end critical
 
-  call find_RZ(node_list,element_list,R_start(i_lines),Z_start(i_lines),R_out,Z_out,i_elm,s_out,t_out,ifail)
+  call find_RZP(node_list,element_list,R_start(i_lines),Z_start(i_lines),P_start(i_lines),R_out,Z_out,i_elm,s_out,t_out,ifail,checked_elms) 
  
   if (ifail .ne. 0) then
     write(*,*) "Can not find RZ,", ifail 
