6762d7102c2c6f2700dc2a92624eabfd3117f2a7
Orin Varley <orin.varley@ipp.mpg.de>
2026-09-07 15:58:01 +0200

    Comment out extra J terms that I meant to leave for now

--- models/model183/mod_equations.f90
+++ models/model183/mod_equations.f90
@@ -556,33 +556,33 @@ module mod_equations
                                         - tstep*theta*v*reta*deta_dT*T*Bv2*zj0*zj0                            ! ohmic heating
       rhs_semianalytic(var_T) = rhs_semianalytic(var_T)                                                                     &
                               + tstep*0.5d0*(v*tauIC*gamma*T0/(Bv2*Bv2*rho0_corr)*Bv_pbrack(rho0*T0,Bv2*rho0)                    &
-                              + v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0)*gradprod(rho0*T0,Psi0)                                &
-                              - v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0*T0)*gradprod(Psi0,rho0)                                &
-                              + v*tauIC*gamma*(Bv_parderiv(T0)/rho0_corr-Bv_parderiv(rho0)*T0/(rho0_corr*rho0_corr))*gradprod(Psi0,rho0)   &
-                              - v*tauIC*gamma*Bv_parderiv(rho0)*(gradprod(Psi0,T0)/rho0_corr-gradprod(Psi0,rho0)*T0/(rho0_corr*rho0_corr)) &
+                              ! + v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0)*gradprod(rho0*T0,Psi0)                                &
+                              ! - v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0*T0)*gradprod(Psi0,rho0)                                &
+                              ! + v*tauIC*gamma*(Bv_parderiv(T0)/rho0_corr-Bv_parderiv(rho0)*T0/(rho0_corr*rho0_corr))*gradprod(Psi0,rho0)   &
+                              ! - v*tauIC*gamma*Bv_parderiv(rho0)*(gradprod(Psi0,T0)/rho0_corr-gradprod(Psi0,rho0)*T0/(rho0_corr*rho0_corr)) &
                               )
 
       amat_semianalytic(var_T, var_T) = amat_semianalytic(var_T, var_T) - tstep*theta*0.5d0*(                                       &
                                         v*tauIC*gamma/(Bv2*Bv2*rho0_corr)*(T*Bv_pbrack(rho0*T0,Bv2*rho0)+T0*Bv_pbrack(rho0*T,Bv2*rho0))  &
-                                      + v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0)*gradprod(Psi0,rho0 * T)                               &
-                                      - v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0*T)*gradprod(Psi0,rho0)                                 &
-                                      + v*tauIC*gamma*(Bv_parderiv(T)/rho0_corr-Bv_parderiv(rho0)*T/(rho0_corr*rho0_corr))*gradprod(Psi0,rho0)     &
-                                      - v*tauIC*gamma*Bv_parderiv(rho0)*(gradprod(Psi0,T)/rho0_corr-gradprod(Psi0,rho0)*T/(rho0_corr*rho0_corr))   &
+                                      ! + v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0)*gradprod(Psi0,rho0 * T)                               &
+                                      ! - v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0*T)*gradprod(Psi0,rho0)                                 &
+                                      ! + v*tauIC*gamma*(Bv_parderiv(T)/rho0_corr-Bv_parderiv(rho0)*T/(rho0_corr*rho0_corr))*gradprod(Psi0,rho0)     &
+                                      ! - v*tauIC*gamma*Bv_parderiv(rho0)*(gradprod(Psi0,T)/rho0_corr-gradprod(Psi0,rho0)*T/(rho0_corr*rho0_corr))   &
                                       )
-      amat_semianalytic(var_T, var_Psi) = amat_semianalytic(var_T, var_Psi) - tstep*theta*0.5d0*(                                   &
-                                          v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0)*gradprod(Psi,rho0*T0)                               &
-                                        - v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0*T0)*gradprod(Psi,rho0)                               &
-                                        + v*tauIC*gamma*(Bv_parderiv(T0)/rho0_corr-Bv_parderiv(rho0)*T0/(rho0_corr*rho0_corr))*gradprod(Psi,rho0)  &
-                                        - v*tauIC*gamma*Bv_parderiv(rho0)*(gradprod(Psi,T0)/rho0_corr-gradprod(Psi,rho0)*T0/(rho0_corr*rho0_corr)) &
-                                        )
+      ! amat_semianalytic(var_T, var_Psi) = amat_semianalytic(var_T, var_Psi) - tstep*theta*0.5d0*(                                   &
+      !                                     v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0)*gradprod(Psi,rho0*T0)                               &
+      !                                   - v*tauIC/(rho0_corr*rho0_corr)*Bv_parderiv(rho0*T0)*gradprod(Psi,rho0)                               &
+      !                                   + v*tauIC*gamma*(Bv_parderiv(T0)/rho0_corr-Bv_parderiv(rho0)*T0/(rho0_corr*rho0_corr))*gradprod(Psi,rho0)  &
+      !                                   - v*tauIC*gamma*Bv_parderiv(rho0)*(gradprod(Psi,T0)/rho0_corr-gradprod(Psi,rho0)*T0/(rho0_corr*rho0_corr)) &
+      !                                   )
       amat_semianalytic(var_T, var_rho) = amat_semianalytic(var_T, var_rho) - tstep*theta*0.5d0*(                         &
                                           v*tauIC*gamma*T0/(Bv2*Bv2*rho0_corr)*(-drho0_corr_dn*rho/rho0_corr*Bv_pbrack(rho0*T0,Bv2*rho0)+Bv_pbrack(T0*rho,Bv2*rho0)+Bv_pbrack(T0*rho0,Bv2*rho)) &
-                                        + v*tauIC/(rho0_corr*rho0_corr)*(-2.d0*drho0_corr_dn*rho/rho0_corr*Bv_parderiv(rho0)*gradprod(rho0*T0,Psi0)+Bv_parderiv(rho)*gradprod(Psi0,rho0*T0)+Bv_parderiv(rho0)*gradprod(Psi0,rho*T0))  &
-                                        - v*tauIC/(rho0_corr*rho0_corr)*(-2.d0*drho0_corr_dn*rho/rho0_corr*Bv_parderiv(rho0*T0)*gradprod(Psi0,rho0)+Bv_parderiv(rho*T0)*gradprod(Psi0,rho0)+Bv_parderiv(rho0*T0)*gradprod(Psi0,rho))  &
-                                        + v*tauIC*gamma*((-drho0_corr_dn*rho/(rho0_corr*rho0_corr)*Bv_parderiv(T0)+2.d0*T0*drho0_corr_dn*rho/(rho0_corr*rho0_corr*rho0_corr)*Bv_parderiv(rho0)-T0/(rho0_corr*rho0_corr)*Bv_parderiv(rho))*gradprod(Psi0,rho0)  &
-                                        + (Bv_parderiv(T0)/rho0_corr-T0/(rho0_corr*rho0_corr)*Bv_parderiv(rho0))*gradprod(Psi0,rho))  &
-                                        - v*tauIC*gamma*(Bv_parderiv(rho)*(gradprod(Psi0,T0)/rho0_corr-gradprod(Psi0,rho0)*T0/(rho0_corr*rho0_corr)) &
-                                        + Bv_parderiv(rho0)*(-drho0_corr_dn*rho/(rho0_corr*rho0_corr)*gradprod(Psi0,T0)+ 2.d0*T0*drho0_corr_dn*rho/(rho0_corr*rho0_corr*rho0_corr)*gradprod(Psi0,rho0) - T0/(rho0_corr*rho0_corr)*gradprod(Psi0,rho))) &
+                                        ! + v*tauIC/(rho0_corr*rho0_corr)*(-2.d0*drho0_corr_dn*rho/rho0_corr*Bv_parderiv(rho0)*gradprod(rho0*T0,Psi0)+Bv_parderiv(rho)*gradprod(Psi0,rho0*T0)+Bv_parderiv(rho0)*gradprod(Psi0,rho*T0))  &
+                                        ! - v*tauIC/(rho0_corr*rho0_corr)*(-2.d0*drho0_corr_dn*rho/rho0_corr*Bv_parderiv(rho0*T0)*gradprod(Psi0,rho0)+Bv_parderiv(rho*T0)*gradprod(Psi0,rho0)+Bv_parderiv(rho0*T0)*gradprod(Psi0,rho))  &
+                                        ! + v*tauIC*gamma*((-drho0_corr_dn*rho/(rho0_corr*rho0_corr)*Bv_parderiv(T0)+2.d0*T0*drho0_corr_dn*rho/(rho0_corr*rho0_corr*rho0_corr)*Bv_parderiv(rho0)-T0/(rho0_corr*rho0_corr)*Bv_parderiv(rho))*gradprod(Psi0,rho0)  &
+                                        ! + (Bv_parderiv(T0)/rho0_corr-T0/(rho0_corr*rho0_corr)*Bv_parderiv(rho0))*gradprod(Psi0,rho))  &
+                                        ! - v*tauIC*gamma*(Bv_parderiv(rho)*(gradprod(Psi0,T0)/rho0_corr-gradprod(Psi0,rho0)*T0/(rho0_corr*rho0_corr)) &
+                                        ! + Bv_parderiv(rho0)*(-drho0_corr_dn*rho/(rho0_corr*rho0_corr)*gradprod(Psi0,T0)+ 2.d0*T0*drho0_corr_dn*rho/(rho0_corr*rho0_corr*rho0_corr)*gradprod(Psi0,rho0) - T0/(rho0_corr*rho0_corr)*gradprod(Psi0,rho))) &
                                         )
                                         
     end if
